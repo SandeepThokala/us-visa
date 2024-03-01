01bee7a4773824f7de74a678aaa2f5cf1c1a1052
SandeepThokala <imdeepu98@gmail.com>
2024-03-01 04:06:25 -0500

    trying github actions

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+import smtplib as smtp
+
+with smtp.SMTP_SSL('smtp.gmail.com', 465) as conn:
+    email_addr = 'sandeep.thokala98@gmail.com'
+    email_passwd = os.environ['APP_PASSWORD']
+    conn.login(email_addr, email_passwd)
+    conn.sendmail(from_addr=email_addr, to_addrs=email_addr, msg=os.environ('MESSAGE')) #"Please check your slot")
