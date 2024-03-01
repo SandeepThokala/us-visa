d790c47db954e3a7df272f2c60f9961ea7b4be4f
SandeepThokala <imdeepu98@gmail.com>
2024-03-01 04:11:27 -0500

    fix message

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ with smtp.SMTP_SSL('smtp.gmail.com', 465) as conn:
     email_addr = 'sandeep.thokala98@gmail.com'
     email_passwd = os.environ['APP_PASSWORD']
     conn.login(email_addr, email_passwd)
-    conn.sendmail(from_addr=email_addr, to_addrs=email_addr, msg=os.environ('MESSAGE')) #"Please check your slot")
+    conn.sendmail(from_addr=email_addr, to_addrs=email_addr, msg=os.environ['MESSAGE']) #"Please check your slot")
